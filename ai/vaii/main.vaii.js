74f8d3a56a1d7a3f1e7b15527236a2d09fc16327
4211421036 <92840324+4211421036@users.noreply.github.com>
2021-12-03 10:21:19 +0700

    Create main.vaii.js

---
+++ ai/vaii/main.vaii.js
@@ -0,0 +1,30 @@
+const speechRecognition =
+      window.webkitSpeechRecognition ||
+      window.SpeechRecognition;
+
+function startListening() {
+  const recog = new speechRecognition();
+  recog.start();
+  recog.onstart = console.log("silakan bicara");
+  
+  recog.onresult = handleResults(data){
+    handleResults(data);
+  };
+}
+
+function handleResults(data){
+  let text = data.results[0][0].transcript;
+  console.log(text);
+  text = text.toLowerCase();
+  
+  if (text.include("ngatuk")){
+    console.log("jangan mengatuk");
+    window.open("https://music.youtube.com/");
+  }
+  else if (text.include("macet")){
+    console.log("jangan marah,. tetap tenang.... dengarin music ini");
+    window.open("https://music.youtube.com/");
+  }
+}
+  
+  window.addEventListener("DOMContentLoaded", startListening());
