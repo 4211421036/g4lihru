e52aeb8e136b23eda3086437070c613776826902
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-01-02 07:38:57 +0700

    Add files via upload

---
+++ src/tfjs/types.ts
@@ -0,0 +1,19 @@
+/** TFJS common types exports */
+
+/**
+ * TensorFlow Tensor type
+ * @external
+ */
+export { Tensor, TensorLike } from '@tensorflow/tfjs-core/dist/index';
+
+/**
+ * TensorFlow GraphModel type
+ * @external
+ */
+export { GraphModel } from '@tensorflow/tfjs-converter/dist/index';
+
+/** Tensorflow Long type
+ * @external long
+ */
+// eslint-disable-next-line node/no-missing-import
+export { Long } from 'long';
