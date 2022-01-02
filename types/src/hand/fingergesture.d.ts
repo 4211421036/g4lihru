4a6db3c540ce75cf1b30d7040d003d135d5cf537
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-01-02 07:42:07 +0700

    Add files via upload

---
+++ types/src/hand/fingergesture.d.ts
@@ -0,0 +1,7 @@
+/**
+ * FingerPose algorithm implementation
+ * See `fingerpose.ts` for entry point
+ */
+import { FingerGesture } from './fingerdef';
+declare const _default: FingerGesture[];
+export default _default;
---
+++ types/src/image/enhance.d.ts
@@ -0,0 +1,5 @@
+/**
+ * Image enhancements
+ */
+import type { Tensor } from '../exports';
+export declare function histogramEqualization(inputImage: Tensor): Promise<Tensor>;
