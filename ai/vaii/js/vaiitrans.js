58653a85c62fadf9720f3d58cbd63b9031ec3ae4
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-02-03 10:21:59 +0700

    Create vaiitrans.js

---
+++ ai/vaii/js/vaiitrans.js
@@ -0,0 +1,8 @@
+class vaiitrans extends HTMLElement {
+    constructor() {
+        super();
+        this.interHTML = `${this.getElementsByClassName('transcript')}`;
+    }
+}
+
+window.customElements.define('vaii-transcript', vaiihint);
