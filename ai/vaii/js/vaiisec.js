20e18ad449d3f19724a8e63e1a35109f547ba844
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-02-03 11:21:10 +0700

    Update vaiisec.js

--- ai/vaii/js/vaiisec.js
+++ ai/vaii/js/vaiisec.js
@@ -865,11 +865,112 @@ vaiitemp.innerHTML = `
         <script src="https://g4lihru.me/ai/vaii/js/vaiilog.js"></script>
         <script src="https://g4lihru.me/ai/vaii/js/vaiires.js"></script>
         <script src="https://g4lihru.me/ai/vaii/js/vaiitrans.js"></script>
+                <div class="glitchButton" style="position:fixed;top:20px;right:20px;"></div>
+        <script type="text/javascript">
+            setTimeout(function() {
+                document.getElementById("loader-wrapper").style.display = "content";
+            }, 2000);
+        </script>
+        <script type="text/javascript">
+            // Get the modal
+            var modal = document.getElementById("myModal");
+
+            // Get the button that opens the modal
+            var btn = document.getElementById("myBtn");
+
+            // Get the <span> element that closes the modal
+            var span = document.getElementsByClassName("close")[0];
+
+            // When the user clicks the button, open the modal 
+            btn.onclick = function() {
+                modal.style.display = "block";
+            }
+
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function() {
+                modal.style.display = "none";
+            }
+
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+        </script>
+        <!-- The core Firebase JS SDK is always required and must be listed first -->
+        <script src="https://www.gstatic.com/firebasejs/7.14.2/firebase-app.js"></script>
+        <!-- TODO: Add SDKs for Firebase products that you want to use
+         https://firebase.google.com/docs/web/setup#available-libraries -->
+        <script src="https://www.gstatic.com/firebasejs/7.14.2/firebase-analytics.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/7.14.2/firebase-firestore.js"></script>
+        <script>
+            // Your web app's Firebase configuration
+            var firebaseConfig = {
+                apiKey: "AIzaSyBvETFg2V6QYmU_TvHN3BWz1TTy4ryZqGM",
+                authDomain: "g4lihriu.firebaseapp.com",
+                databaseURL: "https://g4lihriu-default-rtdb.firebaseio.com",
+                projectId: "g4lihriu",
+                storageBucket: "g4lihriu.appspot.com",
+                messagingSenderId: "387783527536",
+                appId: "1:387783527536:web:8df38ac72783aea162b8b5",
+                measurementId: "G-DCSX3TS7ZS"
+            };
+            // Initialize Firebase
+            firebase.initializeApp(firebaseConfig);
+            firebase.analytics();
+        </script>
+        <script>
+            function myFunction() {
+                var input, filter, ul, li, a, i, txtValue;
+                input = document.getElementById("myInput");
+                filter = input.value.toUpperCase();
+                ul = document.getElementById("myUL");
+                li = ul.getElementsByTagName("li");
+                for (i = 0; i < li.length; i++) {
+                    a = li[i].getElementsByTagName("a")[0];
+                    txtValue = a.textContent || a.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        li[i].style.display = "";
+                    } else {
+                        li[i].style.display = "none";
+                    }
+                }
+            }
+        </script>
+        
+        <script src="https://code.responsivevoice.org/responsivevoice.js?key=BLzc1i7C"></script>
+        <script>
+            let sidebar = document.querySelector(".sidebar");
+            let closeBtn = document.querySelector("#btn");
+            let searchBtn = document.querySelector(".bx-search");
+
+            closeBtn.addEventListener("click", () => {
+                sidebar.classList.toggle("open");
+                menuBtnChange(); //calling the function(optional)
+            });
+
+            searchBtn.addEventListener("click", () => { // Sidebar open when you click on the search iocn
+                sidebar.classList.toggle("open");
+                menuBtnChange(); //calling the function(optional)
+            });
+
+            // following are the code to change sidebar button(optional)
+            function menuBtnChange() {
+                if (sidebar.classList.contains("open")) {
+                    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the iocns class
+                } else {
+                    closeBtn.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the iocns class
+                }
+            }
+        </script>
 `;
 class vaiisec extends HTMLElement {
     constructor() {
         super();
-        this.attachShadow({mode: 'open')};
+        this.attachShadow({
+            mode: 'open'
+        });
         this.shadowRoot.appendChild(vaiitemp.content.clodeNode(true));
         this.shadowRoot.querySelector('div').innerText = this.getElementByClassName('text');
         this.interHTML = `${this.getElementsByClassName('home-section')}`;
