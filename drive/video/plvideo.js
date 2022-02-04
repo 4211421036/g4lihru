6802a734ee69742a35863fc5fd840ef32e5d559b
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-02-05 02:32:52 +0700

    Update plvideo.js

--- drive/video/plvideo.js
+++ drive/video/plvideo.js
@@ -1,3 +1,26 @@
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+let searchBtn = document.querySelector(".bx-search");
+
+closeBtn.addEventListener("click", () => {
+   sidebar.classList.toggle("open");
+   menuBtnChange(); //calling the function(optional)
+});
+
+searchBtn.addEventListener("click", () => { // Sidebar open when you click on the search iocn
+   sidebar.classList.toggle("open");
+   menuBtnChange(); //calling the function(optional)
+});
+
+// following are the code to change sidebar button(optional)
+function menuBtnChange() {
+   if (sidebar.classList.contains("open")) {
+        closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the iocns class
+   } else {
+        closeBtn.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the iocns class
+   }
+}
+
 const template = document.createElement('template');
 template.innerHTML = `
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
@@ -539,28 +562,4 @@ class plvideo extends HTMLElement {
         this.shadowRoot.querySelector('img').src = this.getAttribute('avatar');
     }
 }
-
-let sidebar = document.querySelector(".sidebar");
-            let closeBtn = document.querySelector("#btn");
-            let searchBtn = document.querySelector(".bx-search");
-
-            closeBtn.addEventListener("click", () => {
-                sidebar.classList.toggle("open");
-                menuBtnChange(); //calling the function(optional)
-            });
-
-            searchBtn.addEventListener("click", () => { // Sidebar open when you click on the search iocn
-                sidebar.classList.toggle("open");
-                menuBtnChange(); //calling the function(optional)
-            });
-
-            // following are the code to change sidebar button(optional)
-            function menuBtnChange() {
-                if (sidebar.classList.contains("open")) {
-                    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the iocns class
-                } else {
-                    closeBtn.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the iocns class
-                }
-            }
-
 window.customElements.define('pljr-side', plvideo);
