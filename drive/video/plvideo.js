46946333498c98ff8527263d2e582c9bd6c7ae35
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-02-05 02:27:23 +0700

    Update plvideo.js

--- drive/video/plvideo.js
+++ drive/video/plvideo.js
@@ -461,30 +461,6 @@ template.innerHTML = `
           background: #555; 
         }
         </style>
-        <script>
-            let sidebar = document.querySelector(".sidebar");
-            let closeBtn = document.querySelector("#btn");
-            let searchBtn = document.querySelector(".bx-search");
-
-            closeBtn.addEventListener("click", () => {
-                sidebar.classList.toggle("open");
-                menuBtnChange(); //calling the function(optional)
-            });
-
-            searchBtn.addEventListener("click", () => { // Sidebar open when you click on the search iocn
-                sidebar.classList.toggle("open");
-                menuBtnChange(); //calling the function(optional)
-            });
-
-            // following are the code to change sidebar button(optional)
-            function menuBtnChange() {
-                if (sidebar.classList.contains("open")) {
-                    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the iocns class
-                } else {
-                    closeBtn.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the iocns class
-                }
-            }
-        </script>
         <div class="sidebar">
             <div class="logo-details">
                 <i src="//g4lihru.me/drive/logo.png" class='bx-tada bx-rotate-90'></i>
@@ -563,5 +539,28 @@ class plvideo extends HTMLElement {
         this.shadowRoot.querySelector('img').src = this.getAttribute('avatar');
     }
 }
+
+let sidebar = document.querySelector(".sidebar");
+            let closeBtn = document.querySelector("#btn");
+            let searchBtn = document.querySelector(".bx-search");
+
+            closeBtn.addEventListener("click", () => {
+                sidebar.classList.toggle("open");
+                menuBtnChange(); //calling the function(optional)
+            });
+
+            searchBtn.addEventListener("click", () => { // Sidebar open when you click on the search iocn
+                sidebar.classList.toggle("open");
+                menuBtnChange(); //calling the function(optional)
+            });
+
+            // following are the code to change sidebar button(optional)
+            function menuBtnChange() {
+                if (sidebar.classList.contains("open")) {
+                    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the iocns class
+                } else {
+                    closeBtn.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the iocns class
+                }
+            }
 
 window.customElements.define('pljr-side', plvideo);
