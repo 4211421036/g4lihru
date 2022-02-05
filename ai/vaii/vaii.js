9a79f5e9fe6a86093b568dfd55c5f66c32cae47e
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-02-05 09:55:44 +0700

    Update vaii.js

--- ai/vaii/vaii.js
+++ ai/vaii/vaii.js
@@ -32,52 +32,6 @@ template.innerHTML = `
         .my-float {
             margin-top: 22px;
         }
-
-        .menu {
-            padding: 0;
-        }
-
-        .modal:before {
-            content: '';
-            display: inline-block;
-            height: 100%;
-            vertical-align: middle;
-        }
-
-        .modal-dialog {
-            display: inline-block;
-            vertical-align: middle;
-        }
-
-        .modal .modal-content {
-            padding: 20px 20px 20px 20px;
-            -webkit-animation-name: modal-animation;
-            -webkit-animation-duration: 0.5s;
-            animation-name: modal-animation;
-            animation-duration: 0.5s;
-        }
-
-        @-webkit-keyframes modal-animation {
-            from {
-                top: -100px;
-                opacity: 0;
-            }
-            to {
-                top: 0px;
-                opacity: 1;
-            }
-        }
-
-        @keyframes modal-animation {
-            from {
-                top: -100px;
-                opacity: 0;
-            }
-            to {
-                top: 0px;
-                opacity: 1;
-            }
-        }
     </style>
     <div class="float">
         <iframe class="my-float" src="https://g4lihru.me/ai/vaii/v" width="256" height="256" scrolling="no" allow="autoplay" frameBorder="0"></iframe>
@@ -85,33 +39,6 @@ template.innerHTML = `
     <div class="float1">
         <iframe class="my-float" src="https://g4lihru.me/vcs" width="177" height="176" scrolling="no" allow="autoplay" frameBorder="0"></iframe>
     </div>
-    <script type="text/javascript">
-            // Get the modal
-            var modal = document.getElementById("myModal");
-
-            // Get the button that opens the modal
-            var btn = document.getElementById("myBtn");
-
-            // Get the <span> element that closes the modal
-            var span = document.getElementsByClassName("close")[0];
-
-            // When the user clicks the button, open the modal 
-            btn.onclick = function() {
-                modal.style.display = "block";
-            }
-
-            // When the user clicks on <span> (x), close the modal
-            span.onclick = function() {
-                modal.style.display = "none";
-            }
-
-            // When the user clicks anywhere outside of the modal, close it
-            window.onclick = function(event) {
-                if (event.target == modal) {
-                    modal.style.display = "none";
-                }
-            }
-    </script>
 `;
 
 class vaii extends HTMLElement {
