2708997ef99c94611865c7e73da5319aa1bf8c77
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-02-05 08:36:06 +0700

    Update vaii.js

--- ai/vaii/vaii.js
+++ ai/vaii/vaii.js
@@ -8,20 +8,6 @@ template.innerHTML = `
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://g4lihru.me/meet/dist/tailwind.css" />
     <script src="https://g4lihru.me/script.js" defer></script>
-    <script src="https://g4lihru.me/ai/vaii/js/vaiiside.js"></script>
-    <script src="https://g4lihru.me/ai/vaii/js/vaiiplay.js"></script>
-    <script src="https://g4lihru.me/ai/vaii/js/vaiiground.js"></script>
-    <script src="https://g4lihru.me/ai/vaii/js/vaiiload.js"></script>
-    <script src="https://g4lihru.me/ai/vaii/js/vaiistatus.js"></script>
-    <script src="https://g4lihru.me/ai/vaii/js/vaiibar.js"></script>
-    <script src="https://g4lihru.me/ai/vaii/js/vaiimedia.js"></script>
-    <script src="https://g4lihru.me/ai/vaii/js/vaiicom.js"></script>
-    <script src="https://g4lihru.me/ai/vaii/js/vaiiseg.js"></script>
-    <script src="https://g4lihru.me/ai/vaii/js/vaiisamp.js"></script>
-    <script src="https://g4lihru.me/ai/vaii/js/vaiihint.js"></script>
-    <script src="https://g4lihru.me/ai/vaii/js/vaiilog.js"></script>
-    <script src="https://g4lihru.me/ai/vaii/js/vaiires.js"></script>
-    <script src="https://g4lihru.me/ai/vaii/js/vaiitrans.js"></script>
     <script src="https://g4lihru.me/ai/vaii/index.js" type="module"></script>
     <script src="https://code.responsivevoice.org/responsivevoice.js?key=BLzc1i7C"></script>
     <style>
@@ -424,328 +410,6 @@ template.innerHTML = `
             border-width: 10px 0 8px 10px;
             border-color: transparent transparent transparent white;
         }
-        
-        .modal:before {
-            content: '';
-            display: inline-block;
-            height: 100%;
-            vertical-align: middle;
-        }
-        
-        .modal-dialog {
-            display: inline-block;
-            vertical-align: middle;
-        }
-        
-        .modal .modal-content {
-            padding: 20px 20px 20px 20px;
-            -webkit-animation-name: modal-animation;
-            -webkit-animation-duration: 0.5s;
-            animation-name: modal-animation;
-            animation-duration: 0.5s;
-        }
-        
-        @-webkit-keyframes modal-animation {
-            from {
-                top: -100px;
-                opacity: 0;
-            }
-            to {
-                top: 0px;
-                opacity: 1;
-            }
-        }
-        
-        @keyframes modal-animation {
-            from {
-                top: -100px;
-                opacity: 0;
-            }
-            to {
-                top: 0px;
-                opacity: 1;
-            }
-        }
-        
-        .sidebar {
-            position: fixed;
-            left: 0;
-            top: 0;
-            height: 100%;
-            width: 78px;
-            background: #11101D;
-            padding: 6px 14px;
-            z-index: 99;
-            transition: all 0.5s ease;
-        }
-        
-        .sidebar.open {
-            width: 250px;
-        }
-        
-        .sidebar .logo-details {
-            height: 60px;
-            display: flex;
-            align-items: center;
-            position: relative;
-        }
-        
-        .sidebar .logo-details .icon {
-            opacity: 0;
-            transition: all 0.5s ease;
-        }
-        
-        .sidebar .logo-details .logo_name {
-            color: #fff;
-            font-size: 20px;
-            font-weight: 600;
-            opacity: 0;
-            transition: all 0.5s ease;
-        }
-        
-        .sidebar.open .logo-details .icon,
-        .sidebar.open .logo-details .logo_name {
-            opacity: 1;
-        }
-        
-        .sidebar .logo-details #btn {
-            position: absolute;
-            top: 50%;
-            right: 0;
-            transform: translateY(-50%);
-            font-size: 22px;
-            transition: all 0.4s ease;
-            font-size: 23px;
-            text-align: center;
-            cursor: pointer;
-            transition: all 0.5s ease;
-        }
-        
-        .sidebar.open .logo-details #btn {
-            text-align: right;
-        }
-        
-        .sidebar i {
-            color: #fff;
-            height: 60px;
-            min-width: 50px;
-            font-size: 28px;
-            text-align: center;
-            line-height: 60px;
-        }
-        
-        .sidebar .nav-list {
-            margin-top: 20px;
-            height: 100%;
-        }
-        
-        .sidebar li {
-            position: relative;
-            margin: 8px 0;
-            list-style: none;
-        }
-        
-        .sidebar li .tooltip {
-            position: absolute;
-            top: -20px;
-            left: calc(100% + 15px);
-            z-index: 3;
-            background: #000;
-            box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3);
-            padding: 6px 12px;
-            border-radius: 4px;
-            font-size: 15px;
-            font-weight: 400;
-            opacity: 0;
-            white-space: nowrap;
-            pointer-events: none;
-            transition: 0s;
-        }
-        
-        .sidebar li:hover .tooltip {
-            opacity: 1;
-            pointer-events: auto;
-            transition: all 0.4s ease;
-            top: 50%;
-            transform: translateY(-50%);
-        }
-        
-        .sidebar.open li .tooltip {
-            display: none;
-        }
-        
-        .sidebar input {
-            font-size: 15px;
-            color: #FFF;
-            font-weight: 400;
-            outline: none;
-            height: 50px;
-            width: 100%;
-            width: 50px;
-            border: none;
-            border-radius: 12px;
-            transition: all 0.5s ease;
-            background: #1d1b31;
-        }
-        
-        .sidebar.open input {
-            padding: 0 20px 0 50px;
-            width: 100%;
-        }
-        
-        .sidebar .bx-search {
-            position: absolute;
-            top: 50%;
-            left: 0;
-            transform: translateY(-50%);
-            font-size: 22px;
-            background: #1d1b31;
-            color: #FFF;
-        }
-        
-        .sidebar.open .bx-search:hover {
-            background: #1d1b31;
-            color: #FFF;
-        }
-        
-        .sidebar .bx-search:hover {
-            background: #FFF;
-            color: #11101d;
-        }
-        
-        .sidebar li a {
-            display: flex;
-            height: 100%;
-            width: 100%;
-            border-radius: 12px;
-            align-items: center;
-            text-decoration: none;
-            transition: all 0.4s ease;
-            background: #11101D;
-        }
-        
-        .sidebar li a:hover {
-            background: #FFF;
-        }
-        
-        .sidebar li a .links_name {
-            color: #fff;
-            font-size: 15px;
-            font-weight: 400;
-            white-space: nowrap;
-            opacity: 0;
-            pointer-events: none;
-            transition: 0.4s;
-        }
-        
-        .sidebar.open li a .links_name {
-            opacity: 1;
-            pointer-events: auto;
-        }
-        
-        .sidebar li a:hover .links_name,
-        .sidebar li a:hover i {
-            transition: all 0.5s ease;
-            color: #11101D;
-        }
-        
-        .sidebar li i {
-            height: 50px;
-            line-height: 50px;
-            font-size: 18px;
-            border-radius: 12px;
-        }
-        
-        .sidebar li.profile {
-            position: fixed;
-            height: 60px;
-            width: 78px;
-            left: 0;
-            bottom: -8px;
-            padding: 10px 14px;
-            background: #1d1b31;
-            transition: all 0.5s ease;
-            overflow: hidden;
-        }
-        
-        .sidebar.open li.profile {
-            width: 250px;
-        }
-        
-        .sidebar li .profile-details {
-            display: flex;
-            align-items: center;
-            flex-wrap: nowrap;
-        }
-        
-        .sidebar li img {
-            height: 45px;
-            width: 45px;
-            object-fit: cover;
-            border-radius: 6px;
-            margin-right: 10px;
-        }
-        
-        .sidebar li.profile .name,
-        .sidebar li.profile .job {
-            font-size: 15px;
-            font-weight: 400;
-            color: #fff;
-            white-space: nowrap;
-        }
-        
-        .sidebar li.profile .job {
-            font-size: 12px;
-        }
-        
-        .sidebar .profile #log_out {
-            position: absolute;
-            top: 50%;
-            right: 0;
-            transform: translateY(-50%);
-            background: #1d1b31;
-            width: 100%;
-            height: 60px;
-            line-height: 60px;
-            border-radius: 0px;
-            transition: all 0.5s ease;
-        }
-        
-        .sidebar.open .profile #log_out {
-            width: 50px;
-            background: none;
-        }
-        
-        .home-section {
-            position: relative;
-            background: #E4E9F7;
-            min-height: 100vh;
-            top: 0;
-            left: 78px;
-            width: calc(100% - 78px);
-            transition: all 0.5s ease;
-            z-index: 2;
-        }
-        
-        .sidebar.open~.home-section {
-            left: 250px;
-            width: calc(100% - 250px);
-        }
-        
-        .home-section .text {
-            display: inline-block;
-            color: #11101d;
-            font-size: 25px;
-            font-weight: 500;
-            margin: 18px
-        }
-        
-        @media (max-width: 420px) {
-            .sidebar li .tooltip {
-                display: none;
-            }
-        }
-
         /* width */
         ::-webkit-scrollbar {
           width: 10px;
@@ -770,15 +434,15 @@ template.innerHTML = `
     </style>
     <section class="home-section">
                 <div class="text">VAII</div>
-                <vaii-play disabled id="play" class="play icon-play"></vaii-play>
-                <vaii-ground id="background" class="background">
+                <div disabled id="play" class="play icon-play"></div>
+                <div id="background" class="background">
                     <div class='wave one'></div>
                     <div class='wave two'></div>
                     <div class='wave three'></div>
-                </vaii-ground>
-                <vaii-load id="loader" class="loader"></vaii-load>
-                <vaii-status id="status" class="status"></vaii-status>
-                <vaii-bar id="menubar" class="menubar">
+                </div>
+                <div id="loader" class="loader"></div>
+                <div id="status" class="status"></div>
+                <div id="menubar" class="menubar">
                     <div id="btnDisplay" class="icon">
                         <div class="icon-binoculars"> </div>display</div>
                     <div id="btnImage" class="icon">
@@ -791,8 +455,8 @@ template.innerHTML = `
                         <div id="myBtn" class="icon-record"></div>Start</div>
                     <div id="btnStart" class="icon">
                         <div class="icon-webcam"></div><span id="btnStartText">start video</span></div>
-                </vaii-bar>
-                <vaii-media id="media">
+                </div>
+                <div id="media">
                     <canvas id="canvas" class="canvas" style='position:absolute; left:0px; top:0px;'></canvas>
                         <div class="float">
                             <iframe class="my-float" src="https://g4lihru.me/ai/vaii/v" width="256" height="256" scrolling="no" allow="autoplay" frameBorder="0"></iframe>
@@ -851,20 +515,20 @@ template.innerHTML = `
                         </div>
                     </div>
                     <video id="video" playsinline class="video"></video>
-                </vaii-media>
-                <vaii-compare-container id="compare-container" class="compare-image">
+                </div>
+                <div id="compare-container" class="compare-image">
                     <canvas id="compare-canvas" width="256" height="256"></canvas>
                     <div id="similarity"></div>
-                </vaii-compare-container>
-                <vaii-segmen-container id="segmentation-container" class="compare-image">
+                </div>
+                <div id="segmentation-container" class="compare-image">
                     <canvas id="segmentation-mask" width="256" height="256" style="width: 256px; height: 256px;"></canvas>
                     <canvas id="segmentation-canvas" width="256" height="256 " style="width: 256px; height: 256px;"></canvas>
-                </vaii-segmen-container>
-                <vaii-samp-container id="samples-container" class="samples-container"></vaii-samp-container>
-                <vaii-hint id="hint" class="hint"></vaii-hint>
-                <vaii-log id="log" class="log"></vaii-log>
-                <vaii-results id="results" class="results"></vaii-results>
-                <vaii-transcript id="transcript"></vaii-transcript>
+                </div>
+                <div id="samples-container" class="samples-container"></div>
+                <div id="hint" class="hint"></div>
+                <div id="log" class="log"></div>
+                <div id="results" class="results"></div>
+                <div id="transcript"></div>
             </section>
 `;
 
