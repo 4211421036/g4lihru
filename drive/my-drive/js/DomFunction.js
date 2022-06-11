6ed41a71b522c5f2c1b2dd9a02ee4b8e5b467693
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-06-11 13:04:57 +0700

    Update DomFunction.js

--- drive/my-drive/js/DomFunction.js
+++ drive/my-drive/js/DomFunction.js
@@ -122,14 +122,14 @@ function removefilesharing(id){
 
 var _x = 0
 function openNav() {
-    document.getElementById("left").classList.remove("col-2");
-    document.getElementById("left").classList.remove("p-4");
+    document.getElementById("right").classList.remove("col-2");
+    document.getElementById("right").classList.remove("p-4");
     if(_x == 0){
-        document.getElementById("left").style.width = '0px'
+        document.getElementById("right").style.width = '0px'
         _x=1;
     }else{
         _x = 0
-        document.getElementById("left").style.width = '250px'
+        document.getElementById("right").style.width = '250px'
 
   }
   }
@@ -139,21 +139,21 @@ function openNav() {
       }else{
           _x = 0;
       }
-    document.getElementById("left").classList.remove("col-2");
-    document.getElementById("left").classList.remove("p-4");
-    document.getElementById("left").style.width = '0px'
+    document.getElementById("right").classList.remove("col-2");
+    document.getElementById("right").classList.remove("p-4");
+    document.getElementById("right").style.width = '0px'
   }
 
  window.addEventListener('resize' , function(e){
      if(e.currentTarget.innerWidth > 768){
-    document.getElementById("left").style.width = ''
-    document.getElementById("left").classList.add("col-2");
-    document.getElementById("left").classList.add("p-4");
+    document.getElementById("right").style.width = ''
+    document.getElementById("right").classList.add("col-2");
+    document.getElementById("right").classList.add("p-4");
     
   }else if(e.currentTarget.innerWidth < 768){
-    document.getElementById("left").classList.remove("col-2");
-    document.getElementById("left").classList.remove("p-4");
-    document.getElementById("left").style.width = '0px'
+    document.getElementById("right").classList.remove("col-2");
+    document.getElementById("right").classList.remove("p-4");
+    document.getElementById("right").style.width = '0px'
   }
  }) 
  function FileSize(size) {
