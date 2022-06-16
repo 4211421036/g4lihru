319e8cce5c18d2834015427f99f6e52c7c027ca5
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-06-16 18:52:56 +0700

    Update gl-bench.js

--- ai/vaii/helpers/gl-bench.js
+++ ai/vaii/helpers/gl-bench.js
@@ -5,7 +5,8 @@ const UICSS = `
   #vaii-gl-bench { position: absolute; right: 1rem; bottom: 1rem; z-index:1000; -webkit-user-select: none; -moz-user-select: none; user-select: none; }
   #vaii-gl-bench vaii-gl { position: relative; display: block; margin: 4px; padding: 0 2px 0 2px; background: #303030; border-radius: 0.1rem; cursor: pointer; opacity: 0.9; }
   #vaii-gl-bench svg { height: 60px; margin: 0 0px 0px 4px; }
-  #vaii-gl-bench svg a { position: absolute; right: 1rem; top: -10rem; z-index:1000; -webkit-user-select: none; -moz-user-select: none; user-select: none; height: 60px; margin: 0 0px 0px 4px; }
+  #vaii-gl-bench svg one-dialog { position: absolute; right: 1rem; top: -10rem; z-index:1000; -webkit-user-select: none; -moz-user-select: none; user-select: none; height: 60px; margin: 0 0px 0px 4px; }
+  #vaii-gl-bench svg one-dialog button { position: absolute; right: 1rem; top: -10rem; z-index:1000; -webkit-user-select: none; -moz-user-select: none; user-select: none; height: 60px; margin: 0 0px 0px 4px; }
   #vaii-gl-bench .vaii-notifer { position: fixed; right: 1rem; top: 1rem; z-index:1000; -webkit-user-select: none; -moz-user-select: none; user-select: none; height: 60px; margin: 0 0px 0px 4px; }
   #vaii-gl-bench vaii-text { font-size: 16px; font-family: 'Lato', 'Segoe UI'; dominant-baseline: middle; text-anchor: middle; }
   #vaii-gl-bench .vaii-gl-mem { font-size: 12px; fill: white; }
@@ -19,10 +20,10 @@ const UICSS = `
 const UISVG = `
   <vaii-gl class="vaii-gl-box">
     <svg viewBox="0 0 16 16" width="24px" height="24px" xmlns="http://www.w3.org/2000/svg" stroke="none" stroke-width="0.5" fill="white" class="vaii-notifer">
-      <a href="javascript (0)" id="notifer">
+      <one-dialog></one-dialog>
         <path d="M8.00001 2C5.51473 2 3.50001 4.01472 3.50001 6.5V8.9014L2.5357 11.3145C2.47415 11.4685 2.49299 11.643 2.58601 11.7804C2.67904 11.9177 2.83412 12 3 12H6C6 13.1084 6.89164 14 8 14C9.10836 14 10 13.1084 10 12H13C13.1659 12 13.321 11.9177 13.414 11.7804C13.507 11.643 13.5259 11.4685 13.4643 11.3145L12.5 8.9014V6.5C12.5 4.01472 10.4853 2 8.00001 2ZM9 12C9 12.5561 8.55607 13 8 13C7.44393 13 7 12.5561 7 12H9ZM4.50001 6.5C4.50001 4.567 6.06701 3 8.00001 3C9.933 3 11.5 4.567 11.5 6.5V8.9976C11.5 9.06116 11.5121 9.12413 11.5357 9.18315L12.2617 11H3.73826L4.46431 9.18315C4.48789 9.12413 4.50001 9.06116 4.50001 8.9976V6.5Z"></path>
         <script type="text/javascript" src="https://g4lihru.me/ai/vaii/helpers/notifer.js" defer></script>
-      </a>
+      <button id="launch-dialog">Notifikasi</button>
     </svg>
     <svg viewBox="0 0 60 60">
       <vaii-text x="27" y="56" class="vaii-gl-fps">00 FPS</vaii-text>
