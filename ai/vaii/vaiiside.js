f5edaccd54882b93b659f49c68d97612b1c81a33
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-06-17 06:54:47 +0700

    Update vaiiside.js

--- ai/vaii/vaiiside.js
+++ ai/vaii/vaiiside.js
@@ -1,32 +1,27 @@
-customElements.define( 'vaii-sidebar', class extends HTMLElement 
-{
-  constructor ()
-  {
-    super()
-    this.attachShadow( { mode: 'open'} )
-        .innerHTML = '<slot></slot>'
-  }
-
-  connectedCallback()
-  {        
-    //mousedown anywhere
-    this.mouse_down = ev => !this.contains( ev.target ) && toggle_menu()
-        
-    //toggle menu and window listener 
-    var toggle_menu = () => 
-    {
-      if ( this.classList.toggle( 'show' ) )
-        window.addEventListener( 'mousedown', this.mouse_down )
-      else
-        window.removeEventListener( 'mousedown', this.mouse_down )
+customElements.define('vaii-side', class extends HTMLElement {
+    constructor() {
+        super()
+        this.attachShadow({ mode: 'open' })
+            .innerHTML = '<slot></slot>'
     }
 
-    //click on menu
-    this.addEventListener( 'click', toggle_menu )   
-  }
+    connectedCallback() {
+        //mousedown anywhere
+        this.mouse_down = ev => !this.contains(ev.target) && toggle_menu()
 
-  disconnectedCallback ()
-  {
-    this.removeEventListener( 'mousedown', this.mouse_down )
-  }
-} )
+        //toggle menu and window listener 
+        var toggle_menu = () => {
+            if (this.classList.toggle('show'))
+                window.addEventListener('mousedown', this.mouse_down)
+            else
+                window.removeEventListener('mousedown', this.mouse_down)
+        }
+
+        //click on menu
+        this.addEventListener('click', toggle_menu)
+    }
+
+    disconnectedCallback() {
+        this.removeEventListener('mousedown', this.mouse_down)
+    }
+})
