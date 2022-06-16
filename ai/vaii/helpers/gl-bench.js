72f0bfa2c15a6a1ef451fceb200ab50c47797038
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-06-16 08:57:51 +0000

    Update gl-bench.js

--- ai/vaii/helpers/gl-bench.js
+++ ai/vaii/helpers/gl-bench.js
@@ -12,21 +12,17 @@ const UICSS = `
   #vaii-gl-bench polyline { fill: none; stroke: white; stroke-linecap: round; stroke-linejoin: round; stroke-width: 3.5; }
   #vaii-gl-bench rect { fill: black; }
   #vaii-gl-bench .vaii-opacity { stroke: black; }
-  #vaii-gl-notif-renderer { position: absolute; right: 1rem; top: 1rem; z-index:1000; -webkit-user-select: none; -moz-user-select: none; user-select: none; }
-  #vaii-gl-notif-renderer svg { height: 60px; margin: 0 0px 0px 4px; }
-  #vaii-gl-notif-renderer vaii-menu-notif {.vaii-menu {position: absolute; top: 0rem; right: 1rem; min-width: 180px; width: max-content; padding: 0.2rem 0.8rem 0 0.8rem; line-height: 1.8rem; z-index: 10; background: #303030; border: none;}
-  vaii-menu-notif .tooltip { position: absolute; top: -20px; left: calc(100% + 15px); z-index: 3; background: #000; box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3); padding: 6px 12px; border-radius: 4px; font-size: 15px; font-weight: 400; opacity: 0; white-space: nowrap; pointer-events: none; transition: 0s;}
-  vaii-menu-notif:hover .tooltip { opacity: 1; pointer-events: auto; transition: all 0.4s ease; top: 50%; transform: translateY(-50%);}
+  #vaii-gl-menu { position: absolute; right: 1rem; bottom: 1rem; z-index:1000; -webkit-user-select: none; -moz-user-select: none; user-select: none; }
+  #vaii-gl-menu vaii-menu { position: relative; display: block; margin: 4px; padding: 0 2px 0 2px; background: #303030; border-radius: 0.1rem; cursor: pointer; opacity: 0.9; }
+  #vaii-gl-menu svg { height: 60px; margin: 0 0px 0px 4px; }
   `;
 
 const UISVG = `
   <vaii-gl class="vaii-gl-box">
-    <vaii-menu-notif id="vaii-gl-notif-renderer">
+    <vaii-menu id="vaii-gl-menu>
       <svg viewBox="0 0 16 16" width="24px" height="24px" xmlns="http://www.w3.org/2000/svg" stroke="none" stroke-width="0.5" fill="white">
-          <path d="M8.00001 2C5.51473 2 3.50001 4.01472 3.50001 6.5V8.9014L2.5357 11.3145C2.47415 11.4685 2.49299 11.643 2.58601 11.7804C2.67904 11.9177 2.83412 12 3 12H6C6 13.1084 6.89164 14 8 14C9.10836 14 10 13.1084 10 12H13C13.1659 12 13.321 11.9177 13.414 11.7804C13.507 11.643 13.5259 11.4685 13.4643 11.3145L12.5 8.9014V6.5C12.5 4.01472 10.4853 2 8.00001 2ZM9 12C9 12.5561 8.55607 13 8 13C7.44393 13 7 12.5561 7 12H9ZM4.50001 6.5C4.50001 4.567 6.06701 3 8.00001 3C9.933 3 11.5 4.567 11.5 6.5V8.9976C11.5 9.06116 11.5121 9.12413 11.5357 9.18315L12.2617 11H3.73826L4.46431 9.18315C4.48789 9.12413 4.50001 9.06116 4.50001 8.9976V6.5Z"></path>
+        <path d="M8.00001 2C5.51473 2 3.50001 4.01472 3.50001 6.5V8.9014L2.5357 11.3145C2.47415 11.4685 2.49299 11.643 2.58601 11.7804C2.67904 11.9177 2.83412 12 3 12H6C6 13.1084 6.89164 14 8 14C9.10836 14 10 13.1084 10 12H13C13.1659 12 13.321 11.9177 13.414 11.7804C13.507 11.643 13.5259 11.4685 13.4643 11.3145L12.5 8.9014V6.5C12.5 4.01472 10.4853 2 8.00001 2ZM9 12C9 12.5561 8.55607 13 8 13C7.44393 13 7 12.5561 7 12H9ZM4.50001 6.5C4.50001 4.567 6.06701 3 8.00001 3C9.933 3 11.5 4.567 11.5 6.5V8.9976C11.5 9.06116 11.5121 9.12413 11.5357 9.18315L12.2617 11H3.73826L4.46431 9.18315C4.48789 9.12413 4.50001 9.06116 4.50001 8.9976V6.5Z"></path>
       </svg>
-      <span class="tooltip">Notifikasi</span>
-    <vaii-menu-notif>
   <vaii-gl>
   <vaii-gl class="vaii-gl-box">
     <svg viewBox="0 0 60 60">
