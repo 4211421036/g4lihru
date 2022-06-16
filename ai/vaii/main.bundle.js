e4240104c63860f739477679ceabac2de0eb1569
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-06-16 20:22:35 +0700

    Update main.bundle.js

--- ai/vaii/main.bundle.js
+++ ai/vaii/main.bundle.js
@@ -236,6 +236,126 @@
         disconnectedCallback() { this.removeEventListener("keyup", this.validate.bind(this)) }
         validate() { this.value ? this.style.border = "" : this.style.border = "2px solid red" }
     }
-    firebase.initializeApp({ apiKey: "AIzaSyCgTQkGRQBqmvCY4u6wuJ1MTVQ7YPViUig", authDomain: "web-components-webrtc.firebaseapp.com", databaseURL: "https://web-components-webrtc.firebaseio.com", projectId: "web-components-webrtc", storageBucket: "web-components-webrtc.appspot.com", messagingSenderId: "57628399273", appId: "1:57628399273:web:c01bfb9b6cf5d164007bb8" }), window.customElements.define("vaii-media-router", F), window.customElements.define("vaii-media-home", B), window.customElements.define("vaii-media-about", Ie), window.customElements.define("vaii-media-room", he), window.customElements.define("vaii-media-room-video", be),  customElements.define("vaii-room-chat", Pe),  window.customElements.define("text-input", Te, { extends: "input" })
+    class Oe extends HTMLElement {  
+        constructor() { super(); this.attachShadow({ mode: 'open' }); this.close = this.close.bind(this);}
+        attributeChangedCallback(attrName, oldValue, newValue) {
+            if (oldValue !== newValue) {
+              this[attrName] = this.hasAttribute(attrName);
+            }
+        }  
+        connectedCallback() {
+            const { shadowRoot } = this;
+            shadowRoot.innerHTML = `<style>
+              .wrapper {
+                opacity: 0;
+                transition: visibility 0s, opacity 0.25s ease-in;
+              }
+              .wrapper:not(.open) {
+                visibility: hidden;
+              }
+              .wrapper.open {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+                height: 100vh;
+                position: fixed;
+                  top: 0;
+                  left: 0;
+                  right: 0;
+                  bottom: 0;
+                opacity: 1;
+                visibility: visible;
+              }
+              .overlay {
+                background: rgba(0, 0, 0, 0.8);
+                height: 100%;
+                position: fixed;
+                  top: 0;
+                  right: 0;
+                  bottom: 0;
+                  left: 0;
+                width: 100%;
+              }
+              .dialog {
+                background: #ffffff;
+                max-width: 600px;
+                padding: 1rem;
+                position: fixed;
+              }
+              button {
+                all: unset;
+                cursor: pointer;
+                font-size: 1.25rem;
+                position: absolute;
+                  top: 1rem;
+                  right: 1rem;
+              }
+              button:focus {
+                border: 2px solid blue;
+              }
+            </style>
+            <div class="wrapper">
+            <div class="overlay"></div>
+              <div class="dialog" role="dialog" aria-labelledby="title" aria-describedby="content">
+                <button class="close" aria-label="Close">✖️</button>
+                <h1 id="title">Hello world</h1>
+                <div id="content" class="content">
+                  <p>This is content in the body of our modal</p>
+                </div>
+              </div>
+            </div>`;
+
+
+            shadowRoot.querySelector('button').addEventListener('click', this.close);
+            shadowRoot.querySelector('.overlay').addEventListener('click', this.close);
+            this.open = this.open;
+        }
+  
+       disconnectedCallback() { this.shadowRoot.querySelector('button').removeEventListener('click', this.close); this.shadowRoot.querySelector('.overlay').removeEventListener('click', this.close);}
+  
+       get open() {
+         return this.hasAttribute('open');
+       }
+  
+  
+       set open(isOpen) {
+         const { shadowRoot } = this;
+         shadowRoot.querySelector('.wrapper').classList.toggle('open', isOpen);
+         shadowRoot.querySelector('.wrapper').setAttribute('aria-hidden', !isOpen);
+         if (isOpen) {
+           this._wasFocused = document.activeElement;
+           this.setAttribute('open', '');
+           document.addEventListener('keydown', this._watchEscape);
+           this.focus();
+           shadowRoot.querySelector('button').focus();
+         } else {
+           this._wasFocused && this._wasFocused.focus && this._wasFocused.focus();
+           this.removeAttribute('open');
+           document.removeEventListener('keydown', this._watchEscape);
+           this.close();
+         }
+       }
+
+
+       close() {
+         if (this.open !== false) {
+           this.open = false;
+         }
+         const closeEvent = new CustomEvent('notifer-closed');
+         this.dispatchEvent(closeEvent);
+       }
+
+       _watchEscape(event) {
+         if (event.key === 'Escape') {
+             this.close();   
+         }
+       }
+    }
+
+const button = document.getElementById('notifer');
+button.addEventListener('click', () => {
+  document.querySelector('vaii-menu-notifer').open = true;
+})
+    firebase.initializeApp({ apiKey: "AIzaSyCgTQkGRQBqmvCY4u6wuJ1MTVQ7YPViUig", authDomain: "web-components-webrtc.firebaseapp.com", databaseURL: "https://web-components-webrtc.firebaseio.com", projectId: "web-components-webrtc", storageBucket: "web-components-webrtc.appspot.com", messagingSenderId: "57628399273", appId: "1:57628399273:web:c01bfb9b6cf5d164007bb8" }), window.customElements.define("vaii-media-router", F), window.customElements.define("vaii-media-home", B), window.customElements.define("vaii-media-about", Ie), window.customElements.define("vaii-media-room", he), window.customElements.define("vaii-media-room-video", be),  window.customElements.define("vaii-room-chat", Pe),  window.customElements.define('vaii-menu-notifer', Oe);, window.customElements.define("text-input", Te, { extends: "input" })
 })();
 //# sourceMappingURL=main.bundle.js.map
