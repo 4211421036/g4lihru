aca7bfb6da816e6fbbff2935d93751d24bdd8d24
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-06-18 21:37:50 +0700

    Update vaiilocal.js

--- ai/vaii/vaii-call/vaiilocal.js
+++ ai/vaii/vaii-call/vaiilocal.js
@@ -1,29 +1,3 @@
-localVideo.onmousedown = function(event) {
-    localVideo.style.position = 'absolute';
-    localVideo.style.zIndex = 1000;
-    document.body.appendChild(localVideo);
-
-    moveAt(event.pageX, event.pageY);
-
-    function moveAt(pageX, pageY) {
-        localVideo.style.left = pageX - localVideo.offsetWidth / 2 + 'px';
-        localVideo.style.top = pageY - localVideo.offsetHeight / 2 + 'px';
-    }
-
-    function onMouseMove(event) {
-        moveAt(event.pageX, event.pageY);
-    }
-
-    document.addEventListener('mousemove', onMouseMove);
-
-    localVideo.onmouseup = function() {
-        document.removeEventListener('mousemove', onMouseMove);
-        localVideo.onmouseup = null;
-    };
-
-};
-
-
-localVideo.ondragstart = function() {
-    return false;
-};
+$(function() {
+  $(".drag-me").draggable();
+});
