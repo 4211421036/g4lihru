3b3bd6a4e4abb8559f946d321aa627f80511b11c
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-07-16 20:24:59 +0700

    Update index.js

--- ai/siic/index.js
+++ ai/siic/index.js
@@ -3,7 +3,7 @@ const butInstall = document.getElementById('butInstall');
 /* Put code here */
 /* Only register a service worker if it's supported */
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/ai/siic/service-worker.js');
+  navigator.serviceWorker.register('service-worker.js');
 }
 
 /**
