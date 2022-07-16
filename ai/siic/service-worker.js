dc86d2b617d050b587c97d25d3c44ace3b8cd845
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-07-16 22:32:37 +0700

    Update service-worker.js

--- ai/siic/service-worker.js
+++ ai/siic/service-worker.js
@@ -1,52 +1,33 @@
-const CACHE_NAME = 'SIIC';
-const OFFLINE_URL = 'ai/siic/';
+const siic = "SIIC"
+const assets = [
+  "/",
+  "/ai/siic/index.html",
+  "/ai/siic/css/leaflet.css",
+  "/987654567.png",
+  "/ai/siic/css/css-font-awesome.min.css",
+  "/ai/siic/css/assets-owl.carousel.min.css",
+  "/ai/siic/css/venobox-venobox.css",
+  "/ai/siic/css/aos-aos.css",
+  "/ai/siic/css/css-style.css",
+  "/ai/siic/lef.js",
+  "/ai/siic/fonts-boxicons.eot",
+  "/ai/siic/fonts-boxicons.svg",
+  "/ai/siic/fonts-boxicons.ttf",
+  "/ai/siic/fonts-boxicons.woff",
+  "/ai/siic/fonts-boxicons.woff2",
+  "/ai/siic/fonts-fontawesome-webfont.eot",
+  "/ai/siic/fonts-fontawesome-webfont.svg",
+  "/ai/siic/fonts-fontawesome-webfont.ttf",
+  "/ai/siic/fonts-fontawesome-webfont.woff",
+  "/ai/siic/fonts-fontawesome-webfont.woff2",
+  "/ai/siic/img-preloader.svg",
+  "/ai/siic/index.js",
+]
 
-self.addEventListener('install', function(event) {
-  console.log('[ServiceWorker] Install');
-  
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    // Setting {cache: 'reload'} in the new request will ensure that the response
-    // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
-    await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
-  })());
-  
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('[ServiceWorker] Activate');
-  event.waitUntil((async () => {
-    // Enable navigation preload if it's supported.
-    // See https://developers.google.com/web/updates/2017/02/navigation-preload
-    if ('navigationPreload' in self.registration) {
-      await self.registration.navigationPreload.enable();
-    }
-  })());
-
-  // Tell the active service worker to take control of the page immediately.
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', function(event) {
-  // console.log('[Service Worker] Fetch', event.request.url);
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResponse = await event.preloadResponse;
-        if (preloadResponse) {
-          return preloadResponse;
-        }
-
-        const networkResponse = await fetch(event.request);
-        return networkResponse;
-      } catch (error) {
-        console.log('[Service Worker] Fetch failed; returning offline page instead.', error);
-
-        const cache = await caches.open(CACHE_NAME);
-        const cachedResponse = await cache.match(OFFLINE_URL);
-        return cachedResponse;
-      }
-    })());
-  }
-});
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(siic).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
