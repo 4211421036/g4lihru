cb5297350ec8130392ede019a003787267dd9cb3
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-07-16 23:40:22 +0700

    Update service-worker.js

--- ai/siic/service-worker.js
+++ ai/siic/service-worker.js
@@ -1,27 +1,5 @@
 const siic = "SIIC";
-const assets = [
-  "/",
-  "/index.html",
-  "/css/leaflet.css",
-  "/css-font-awesome.min.css",
-  "/css/assets-owl.carousel.min.css",
-  "/css/venobox-venobox.css",
-  "/css/aos-aos.css",
-  "/css/css-style.css",
-  "/lef.js",
-  "/fonts/fonts-boxicons.eot",
-  "/fonts/fonts-boxicons.svg",
-  "/fonts/fonts-boxicons.ttf",
-  "/fonts/fonts-boxicons.woff",
-  "/fonts/fonts-boxicons.woff2",
-  "/fonts/fonts-fontawesome-webfont.eot",
-  "/fonts/fonts-fontawesome-webfont.svg",
-  "/fonts/fonts-fontawesome-webfont.ttf",
-  "/fonts/fonts-fontawesome-webfont.woff",
-  "/fonts/fonts-fontawesome-webfont.woff2",
-  "/fonts/img-preloader.svg",
-  "/index.js",
-];
+const assets = 'ai/siic/';
 
 self.addEventListener('install', function(event) {
   console.log('[ServiceWorker] Install');
