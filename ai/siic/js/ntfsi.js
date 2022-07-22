25fa026a2cbca19c536b967fdec9759b52480a73
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-07-22 10:44:33 +0700

    Create ntfsi.js

---
+++ ai/siic/js/ntfsi.js
@@ -0,0 +1,31 @@
+function SIICNotif() {
+    if (!("Notification" in window)) {
+        alert("This browser does not support system notifications");
+    } else if (Notification.permission === "granted") {
+        notify();
+    } else if (Notification.permission !== 'denied') {
+        Notification.requestPermission(function(permission) {
+            if (permission === "granted") {
+                notify();
+            }
+        });
+    }
+
+    function notify() {
+        var notification = new Notification('Notifikasi SIIC', {
+            icon: 'https://g4lihru.me/345677.png',
+            body: 'Perkembangan! Jalanan yang akan dilalui anda diprediksi macet. /n Anda bisa beralih ke transportasi umum!',
+            image: "https://g4lihru.me/ai/recis/recis.-2143340583627970034.gif",
+            badge: "https://g4lihru.me/345677.png",
+            vibrate: [200, 100, 200],
+            tag: "Transportasi Umum, Murah dan Nyaman!",
+        });
+        notification.onclick = function() {
+            event.preventDefault(); // prevent the browser from focusing the Notification's tab
+            window.open("https://g4lihru.me/ai/recis", '_blank');
+        };
+        setTimeout(notification.close.bind(notification), 7000);
+    }
+
+}
+SIICNotif();
