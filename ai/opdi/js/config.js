e1f4712d1bff9b86972293d5d07d36ae553a5ea5
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-07-29 15:45:35 +0700

    Create config.js

---
+++ ai/opdi/js/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBvETFg2V6QYmU_TvHN3BWz1TTy4ryZqGM",
+    authDomain: "g4lihriu.firebaseapp.com",
+    databaseURL: "https://g4lihriu-default-rtdb.firebaseio.com",
+    projectId: "g4lihriu",
+    storageBucket: "g4lihriu.appspot.com",
+    messagingSenderId: "387783527536",
+    appId: "1:387783527536:web:8df38ac72783aea162b8b5",
+    measurementId: "G-DCSX3TS7ZS"  
+ };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
