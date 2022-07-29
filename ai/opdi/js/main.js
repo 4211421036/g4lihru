861e048b316e423c6d8f706da6cfc4b69d2dcac4
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-07-29 15:47:12 +0700

    Create main.js

---
+++ ai/opdi/js/main.js
@@ -0,0 +1,36 @@
+const auth = firebase.auth();
+var rand = Math.floor((Math.random() * 99999999999) + 1);
+//----------------user if loggined---------------------//
+auth.onAuthStateChanged(function(user){
+		
+    if(user){
+    console.log('User LoggedIn')
+       	uid = user.uid;
+       	localStorage.setItem("uid",uid); 
+	loadfolders();
+	$('.user-info img').show();
+	$('.user-info img').attr('src', user.photoUrl);
+	$('.user-info .user-name').hide();
+	uid = user.uid;
+        $('.user-info img').hide();
+	$('.user-info').append('<span class="user-name">' + user.username + '</span>');
+	$('.user-info img').hide();
+	$('.user-info').append('<span class="user-name">' + user.username + '</span>');
+      
+    }else{
+        console.log("No Active User");
+        //no user is signed in
+        window.location.replace("login.html");
+    }
+});
+function get($){
+return document.querySelector($);
+}
+window.addEventListener('load',function(){
+    if(window.innerWidth < 768){
+        document.getElementById("left").classList.remove("col-2");
+        document.getElementById("left").classList.remove("p-4");
+        document.getElementById("left").style.width = '0px'
+    }
+})
+
