60bd82bc9287bbd9955578e1ac23ca4d1fb55ea0
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-08-03 05:39:19 +0700

    Update opdi.js

--- ai/opdi/js/opdi.js
+++ ai/opdi/js/opdi.js
@@ -37,14 +37,6 @@ class OpdiApp extends HTMLElement {
         // Create a shadow root
         const shadow = this.attachShadow({ mode: 'open' });
 
-        // Create header
-        const header = document.createElement('opdi-header');
-        header.setAttribute('class', 'header');
-
-        const icon = document.createElement('opdi-header-home');
-        icon.setAttribute('class', 'home');
-        icon.setAttribute('tabindex', 0);
-
         const shd = document.createElement('opdi-side')
         shd.setAttribute('class', 'screen')
 
@@ -95,42 +87,6 @@ class OpdiApp extends HTMLElement {
         likHome3.innerText = 'Hall'
         basHom3.appendChild(likHome3)
 
-        const info = document.createElement('opdi-header-info');
-        info.setAttribute('class', 'app-all');
-
-        // Take attribute content and put it inside the info span
-        const text = this.getAttribute('data-text');
-        info.textContent = text;
-
-        // Insert icon
-        let imgUrl;
-        if (this.hasAttribute('img')) {
-            imgUrl = this.getAttribute('img');
-        } else {
-            imgUrl = 'home.svg';
-        }
-
-        const img = document.createElement('img');
-        img.setAttribute('class', 'all-home')
-        img.src = imgUrl;
-        icon.appendChild(img);
-
-        let imgAUrl;
-        if (this.hasAttribute('img')) {
-            imgAUrl = this.getAttribute('img');
-        } else {
-            imgAUrl = 'app.svg';
-        }
-
-        const butapp = document.createElement('button')
-        butapp.setAttribute('data-link', '')
-        butapp.setAttribute('class', 'butapp')
-
-        const imgAll = document.createElement('img');
-        imgAll.setAttribute('class', 'all-apps')
-        imgAll.src = imgAUrl;
-        butapp.appendChild(imgAll);
-
         // Apply external styles to the shadow dom
         const linkElem = document.createElement('link');
         linkElem.setAttribute('rel', 'stylesheet');
@@ -138,13 +94,9 @@ class OpdiApp extends HTMLElement {
 
         // Attach the created elements to the shadow dom
         shadow.appendChild(linkElem);
-        shadow.appendChild(header);
         shadow.appendChild(shd);
         shadow.appendChild(navs);
         shadow.appendChild(mapbas);
-        header.appendChild(icon);
-        header.appendChild(info);
-        info.appendChild(butapp);
     }
 }
 
