47b0321a95323478067ed321b93a10edce5a1ef2
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-08-05 14:50:15 +0700

    Update opdi.js

--- ai/opdi/js/opdi.js
+++ ai/opdi/js/opdi.js
@@ -56,7 +56,7 @@ class OpdiApp extends HTMLElement {
         const AB = document.createElement("a");
         const AC = document.createElement("bar-side");
         AC.setAttribute("class", "items"), r.appendChild(AC);
-        AB.href = "#reward", A.innerText = "Reward", AC.appendChild(AB);
+        AB.href = "#reward", AB.innerText = "Reward", AC.appendChild(AB);
         const T = document.createElement("link");
         T.setAttribute("rel", "stylesheet"), T.setAttribute("href", "style.css"), e.appendChild(T), e.appendChild(t), e.appendChild(r)
     }
