c4186b13ab91133c9df6e612a3bacc139c92dae0
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-08-08 10:46:48 +0700

    Update eyelans.js

--- ai/opdi/js/eyelans.js
+++ ai/opdi/js/eyelans.js
@@ -60,7 +60,7 @@ class OpdiApp extends HTMLElement {
         j.setAttribute('class', 'logo-text'), j.innerText = 'CONTACT LENS', k.appendChild(j)
         const i = document.createElement("opdi-search");
         let s, q, lp, xi, v, mi;
-        i.setAttribute("class", "search"), t.appendChild(i), s = this.hasAttribute("img") ? this.getAttribute("img") : "../data/icon/profile.svg", lp = this.hasAttribute("img") ? this.getAttribute("img") : "../data/icon/search.svg", xi = this.hasAttribute("img") ? this.getAttribute("img") : "../data/icon/eyeim.svg",mi = this.hasAttribute("img") ? this.getAttribute("img") : "../data/icon/eyelaco.svg", v = this.hasAttribute("img") ? this.getAttribute("img") : "../data/icon/back.svg", q = this.hasAttribute("img") ? this.getAttribute("img") : "../data/icon/eyelaco.svg";
+        i.setAttribute("class", "search"), t.appendChild(i), s = this.hasAttribute("img") ? this.getAttribute("img") : "../data/icon/profile.svg", lp = this.hasAttribute("img") ? this.getAttribute("img") : "../data/icon/search.svg", xi = this.hasAttribute("img") ? this.getAttribute("img") : "../data/icon/eyeim.svg",mi = this.hasAttribute("img") ? this.getAttribute("img") : "../data/icon/eyelacon.svg", v = this.hasAttribute("img") ? this.getAttribute("img") : "../data/icon/back.svg", q = this.hasAttribute("img") ? this.getAttribute("img") : "../data/icon/eyelaco.svg";
         const c = document.createElement("input");
         c.setAttribute("class", "img-search"), c.type = "text", c.onkeyup = "myFunction()", c.placeholder = "Search Menu", c.title = "Search", c.setAttribute("id", "Input-Opdi"), i.appendChild(c);
         const um = document.createElement("img");
