d268e2ebffde6661bc081a01515068134b6cb305
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-08-08 12:47:36 +0700

    Update eyechal.js

--- ai/opdi/js/eyechal.js
+++ ai/opdi/js/eyechal.js
@@ -9,6 +9,33 @@ function back() {
     window.location.replace("/ai/opdi");
 }
 
+function eyetes() {
+    window.location.replace("/ai/opdi/eyetes/");
+}
+function eyelans() {
+    window.location.replace("/ai/opdi/eyelans/");
+}
+function eyefac() {
+    window.location.replace("/ai/opdi/eyefac/");
+}
+function eyedoc() {
+    window.location.replace("/ai/opdi/eyedoc/");
+}
+function eyevit() {
+    window.location.replace("/ai/opdi/eyevit/");
+}
+function eyeglas() {
+    window.location.replace("/ai/opdi/eyeglas/");
+}
+function eyeque() {
+    window.location.replace("/ai/opdi/eyeque/");
+}
+function eyehel() {
+    window.location.replace("/ai/opdi/eyehel/");
+}
+function eyechal() {
+    window.location.replace("/ai/opdi/eyechal/");
+}
 
 function myFunction() { var e, t, n, a; for (e = document.getElementById("Input-Opdi").value.toUpperCase(), t = document.getElementById("map-bar").getElementsByTagName("bar-side"), a = 0; a < t.length; a++)((n = t[a].getElementsByTagName("a")[0]).textContent || n.innerText).toUpperCase().indexOf(e) > -1 ? t[a].style.display = "" : t[a].style.display = "none" }
 class OpdiApp extends HTMLElement {
@@ -17,6 +44,10 @@ class OpdiApp extends HTMLElement {
         const e = this.attachShadow({ mode: "open" }),
             t = document.createElement("opdi-side");
         t.setAttribute("class", "screen");
+        const r = document.createElement("odpi-icon");
+        const tr = document.createElement("opdi-text");
+        tr.setAttribute('class', 'scope-text'), tr.id = 'scope-tx', tr.innerText = 'pada fitur ini kamu diharuskan melakukan test mata yang tertera disini, kamu ada dites perhari seberapa sehat matamu challange ini hanya untuk visual acuity';
+        r.appendChild(tr)
         const n = document.createElement("opdi-title");
         n.setAttribute("class", "profile"), t.appendChild(n);
         const l = document.createElement("opdi-back");
@@ -26,12 +57,15 @@ class OpdiApp extends HTMLElement {
         k.setAttribute('class', 'scope-log')
         t.appendChild(k);
         const j = document.createElement('opdi-text');
-        j.setAttribute('class', 'logo-text'), j.innerText = 'GLASSES', k.appendChild(j)
+        j.setAttribute('class', 'logo-text'), j.innerText = 'CHALLANGE', k.appendChild(j)
         const i = document.createElement("opdi-search");
-        let s, q, lp, xi, v;
-        i.setAttribute("class", "search"), t.appendChild(i), s = this.hasAttribute("img") ? this.getAttribute("img") : "https://g4lihru.me/ai/opdi/data/icon/profile.svg", lp = this.hasAttribute("img") ? this.getAttribute("img") : "https://g4lihru.me/ai/opdi/data/icon/search.svg", xi = this.hasAttribute("img") ? this.getAttribute("img") : "https://g4lihru.me/ai/opdi/data/icon/eyeim.svg", v = this.hasAttribute("img") ? this.getAttribute("img") : "https://g4lihru.me/ai/opdi/data/icon/back.svg", q = this.hasAttribute("img") ? this.getAttribute("img") : "https://g4lihru.me/ai/opdi/data/icon/eyeglas.svg";
+        let s, q, lp, xi, v, mi;
+        i.setAttribute("class", "search"), t.appendChild(i), s = this.hasAttribute("img") ? this.getAttribute("img") : "../data/icon/profile.svg", lp = this.hasAttribute("img") ? this.getAttribute("img") : "../data/icon/search.svg", xi = this.hasAttribute("img") ? this.getAttribute("img") : "../data/icon/eyeim.svg",mi = this.hasAttribute("img") ? this.getAttribute("img") : "../data/icon/eyecha.svg", v = this.hasAttribute("img") ? this.getAttribute("img") : "../data/icon/back.svg", q = this.hasAttribute("img") ? this.getAttribute("img") : "../data/icon/eyechco.svg";
         const c = document.createElement("input");
         c.setAttribute("class", "img-search"), c.type = "text", c.onkeyup = "myFunction()", c.placeholder = "Search Menu", c.title = "Search", c.setAttribute("id", "Input-Opdi"), i.appendChild(c);
+        const um = document.createElement("img");
+        um.setAttribute("class", "scope-ico",), um.id = 'scope-co',
+            um.alt = "Logo", um.src = mi, r.appendChild(um);
         const uv = document.createElement("img");
         uv.setAttribute("class", "back", ), uv.id = 'back',
             uv.alt = "cricle", uv.src = v, l.appendChild(uv);
@@ -47,7 +81,7 @@ class OpdiApp extends HTMLElement {
         lo.setAttribute("class", "img-searc"), lo.id = "img-sear"
         lo.alt = "search", lo.src = lp, i.appendChild(lo);
         const T = document.createElement("link");
-        T.setAttribute("rel", "stylesheet"), T.setAttribute("href", "style.css"), e.appendChild(T), e.appendChild(t)
+        T.setAttribute("rel", "stylesheet"), T.setAttribute("href", "style.css"), e.appendChild(T), e.appendChild(t), e.appendChild(r)
     }
 }
 customElements.define("opdi-app", OpdiApp);
