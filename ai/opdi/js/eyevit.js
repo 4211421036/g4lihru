0af36b691d3130c6a1b512c1e2af56911f95c059
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-08-09 07:41:50 +0700

    Update eyevit.js

--- ai/opdi/js/eyevit.js
+++ ai/opdi/js/eyevit.js
@@ -1,12 +1,39 @@
 let opdi = document.createElement("opdi-app");
 document.body.appendChild(opdi);
 let cfg = document.createElement("script");
-cfg.setAttribute("src", "../js/config.js"), document.body.appendChild(cfg);
+cfg.setAttribute("src", "js/config.js"), document.body.appendChild(cfg);
 let main = document.createElement("script");
-main.setAttribute("src", "../js/main.js"), document.body.appendChild(main);
+main.setAttribute("src", "js/main.js"), document.body.appendChild(main);
 
 function back() {
     window.location.replace("/ai/opdi");
+}
+function eyetes() {
+    window.location.replace("/ai/opdi/eyetes/");
+}
+function eyelans() {
+    window.location.replace("/ai/opdi/eyelans/");
+}
+function eyefac() {
+    window.location.replace("/ai/opdi/eyefac/");
+}
+function eyedoc() {
+    window.location.replace("/ai/opdi/eyedoc/");
+}
+function eyevit() {
+    window.location.replace("/ai/opdi/eyevit/");
+}
+function eyeglas() {
+    window.location.replace("/ai/opdi/eyeglas/");
+}
+function eyeque() {
+    window.location.replace("/ai/opdi/eyeque/");
+}
+function eyehel() {
+    window.location.replace("/ai/opdi/eyehel/");
+}
+function eyechal() {
+    window.location.replace("/ai/opdi/eyechal/");
 }
 
 
@@ -17,6 +44,16 @@ class OpdiApp extends HTMLElement {
         const e = this.attachShadow({ mode: "open" }),
             t = document.createElement("opdi-side");
         t.setAttribute("class", "screen");
+        const r = document.createElement("opdi-icon");
+        const y = document.createElement("opdi-text");
+        y.innerText = 'menjaga kesehatan sangat perlu dilakukan agar mata tetap bisa melihat dengan normal tapi untuk mencapai hal itu kita harus memakan makanan yang bergizi terutama pada vitamiin', y.className = "scope-text"
+        r.appendChild(y);
+        const xq = document.createElement("opdi-shop");
+        xq.id = "scope-shop", xq.className = "scope-shope",
+            e.appendChild(xq)
+        const xe = document.createElement("opdi-text")
+        xe.innerText = "Pesan Sekarang", xe.className = "scope-text", xe.id = "scope-text",
+            xq.appendChild(xe)
         const n = document.createElement("opdi-title");
         n.setAttribute("class", "profile"), t.appendChild(n);
         const l = document.createElement("opdi-back");
@@ -26,20 +63,23 @@ class OpdiApp extends HTMLElement {
         k.setAttribute('class', 'scope-log')
         t.appendChild(k);
         const j = document.createElement('opdi-text');
-        j.setAttribute('class', 'logo-text'), j.innerText = 'GLASSES', k.appendChild(j)
+        j.setAttribute('class', 'logo-text'), j.innerText = 'VITAMIN', k.appendChild(j)
         const i = document.createElement("opdi-search");
-        let s, q, lp, xi, v;
-        i.setAttribute("class", "search"), t.appendChild(i), s = this.hasAttribute("img") ? this.getAttribute("img") : "https://g4lihru.me/ai/opdi/data/icon/profile.svg", lp = this.hasAttribute("img") ? this.getAttribute("img") : "https://g4lihru.me/ai/opdi/data/icon/search.svg", xi = this.hasAttribute("img") ? this.getAttribute("img") : "https://g4lihru.me/ai/opdi/data/icon/eyeim.svg", v = this.hasAttribute("img") ? this.getAttribute("img") : "https://g4lihru.me/ai/opdi/data/icon/back.svg", q = this.hasAttribute("img") ? this.getAttribute("img") : "https://g4lihru.me/ai/opdi/data/icon/eyeglas.svg";
+        let s, q, lp, xi, v, mi;
+        i.setAttribute("class", "search"), t.appendChild(i), s = this.hasAttribute("img") ? this.getAttribute("img") : "profile.svg", lp = this.hasAttribute("img") ? this.getAttribute("img") : "search.svg", xi = this.hasAttribute("img") ? this.getAttribute("img") : "eyeim.svg", v = this.hasAttribute("img") ? this.getAttribute("img") : "back.svg", mi = this.hasAttribute("img") ? this.getAttribute("img") : "vico.svg", q = this.hasAttribute("img") ? this.getAttribute("img") : "vic.svg";
         const c = document.createElement("input");
         c.setAttribute("class", "img-search"), c.type = "text", c.onkeyup = "myFunction()", c.placeholder = "Search Menu", c.title = "Search", c.setAttribute("id", "Input-Opdi"), i.appendChild(c);
+        const um = document.createElement("img");
+        um.setAttribute("class", "scope-ico",), um.id = 'scope-co',
+            um.alt = "cricle", um.src = mi, r.appendChild(um);
         const uv = document.createElement("img");
-        uv.setAttribute("class", "back", ), uv.id = 'back',
+        uv.setAttribute("class", "back",), uv.id = 'back',
             uv.alt = "cricle", uv.src = v, l.appendChild(uv);
         const ui = document.createElement("img");
-        ui.setAttribute("class", "circ", ), ui.id = 'circ',
+        ui.setAttribute("class", "circ",), ui.id = 'circ',
             ui.alt = "cricle", ui.src = xi, n.appendChild(ui);
         const m = document.createElement("img");
-        m.setAttribute("class", "img-profile myprofile", ), m.id = 'profile',
+        m.setAttribute("class", "img-profile myprofile",), m.id = 'profile',
             m.alt = "profile", m.src = s, n.appendChild(m);
         const w = document.createElement("img");
         w.setAttribute("class", "logo-gla"), w.alt = "profile", w.src = q, k.appendChild(w);
@@ -47,7 +87,7 @@ class OpdiApp extends HTMLElement {
         lo.setAttribute("class", "img-searc"), lo.id = "img-sear"
         lo.alt = "search", lo.src = lp, i.appendChild(lo);
         const T = document.createElement("link");
-        T.setAttribute("rel", "stylesheet"), T.setAttribute("href", "style.css"), e.appendChild(T), e.appendChild(t)
+        T.setAttribute("rel", "stylesheet"), T.setAttribute("href", "style.css"), e.appendChild(T), e.appendChild(t), e.appendChild(r)
     }
 }
 customElements.define("opdi-app", OpdiApp);
