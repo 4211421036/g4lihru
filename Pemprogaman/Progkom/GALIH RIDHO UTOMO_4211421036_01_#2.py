22e98a2495326e2cbee603e0e538845d5167ffab
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-10-06 06:00:10 +0700

    Create GALIH RIDHO UTOMO_4211421036_01_#2.py

---
+++ Pemprogaman/Progkom/GALIH RIDHO UTOMO_4211421036_01_#2.py
@@ -0,0 +1,20 @@
+#GALIH RIDHO UTOMO
+#4211421036
+#FISIKA MURNI
+#SOAL NO 1 Menentukan Jumlah SKS
+
+print ('''
+Menentukan Jumlah SKS 
+berdasarkan IPK
+''')
+
+
+ipk = input("Jumlah IPK: ")
+if ipk >= "3" or ipk == "3":
+    print("Anda berhak mengambil 24 SKS")
+elif ipk <= "3":
+    print("\nMohon maaf anda belum bisa mengambil 24 SKS, untuk sekarang ini anda 22 SKS")
+print("\nJumlah SKS yang telah ditentukan dengan IPK anda", ipk)
+print ('''
+Selamat anda telah mengetahui jumlah SKS yang diambil
+''')
