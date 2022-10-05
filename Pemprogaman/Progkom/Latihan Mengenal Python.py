2432741c79e8ea42b9e4f0298a5c271e51dc4c19
4211421036 <92840324+4211421036@users.noreply.github.com>
2022-10-06 06:03:46 +0700

    Create  Latihan Mengenal Python.py

---
+++ Pemprogaman/Progkom/Latihan Mengenal Python.py
@@ -0,0 +1,13 @@
+#Nama: GALIH RIDHO UTOMO
+#NIM: 4211421036
+#Prodi: Fisika Murni
+
+nama = input("Masukan nama anda: ")
+nim = int(input("Masukan nim anda: (10 digit angka)"))
+ttl = input("Asal: ")
+
+if ttl == ttl:
+    print("Nama saya: \n", nama, "\nNIM saya adalah...\n", nim, "\nsaya lahir di kota mana\n", "kota", ttl)
+else:
+    print("Isi data dengan benar")
+    quit()
